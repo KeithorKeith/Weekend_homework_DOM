a3fbb82dd4a32207bc0f5dd03a442c4ec73a7189
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-06-19 19:31:45 +0100

    In process of writing functionality to add a list item.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const newItemForm = document.querySelector('#new-item-form');
-//     newItemForm.addEventListener('submit', handleNewItemFormSubmit);
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    const newItemForm = document.querySelector('#new-item-form');
+    newItemForm.addEventListener('submit', handleNewItemFormSubmit);
+})
+
+const handleNewItemFormSubmit = function (event) {
+    event.preventDefault();
+
+    const partyListItem = createPartyListItem(event.target);
+    const partyList = document .querySelector('#party-list');
+    partyList.appendChild(partyListItem);
+
+    event.target.reset();
+}
+
+const createPartyListItem = function (form) {
+    const partyListItem = document.createElement('li');
+    
+}
